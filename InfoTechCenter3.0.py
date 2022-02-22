59e4bf2d3a35d5e406d17e808089036a295f4f2a
TylerShepard <02tyler.shepard@gulllakecs.org>
2022-02-22 11:17:38 -0800

    Changed Miles variable from an Int. to A Float

--- InfoTechCenter3.0.py
+++ InfoTechCenter3.0.py
@@ -12,20 +12,20 @@ gasLevelIndicator = gasLevelGauge()
 # Create If-ELIF-ELSE statements the Comparative Operator == Equal To in order to display gas level messages
 def gasLevelAlert():
     gasStations = ["Shell","BP","Citgo","Circle K","Mobil","Speedway","Marathon","Love's","Meijer","Costco","Sunoco"]
-    miles = random.randint(1, 25)
+    miles = round(random.uniform(1, 25), 2)
     if gasLevelIndicator == "Empty":
-        print("   WARNING YOU ARE ON EMPTY\nCalling Emergency Contact")
+        print("   WARNING YOU ARE ON EMPTY\n Calling Emergency Contact")
     elif gasLevelIndicator == "Low":
-        print("      WARNING\nYour gas tank is low on gas\nThe closest gasoline station is " + random.choice(gasStations) + " which is " + str(miles) + " miles away")
+        print("      WARNING\n Your gas tank is low on gas\n The closest gasoline station is " + random.choice(gasStations) + " which is " + str(miles) + " miles away")
     elif gasLevelIndicator == "Quarter Tank":
-        print("You have a quarter tank of gas left\nThe closest gasoline station is " + random.choice(gasStations) + " which is " + str(miles) + " miles away")
+        print("     You have a quarter tank of gas left\n The closest gasoline station is " + random.choice(gasStations) + " which is " + str(miles) + " miles away")
 
     elif gasLevelIndicator == "Half Tank":
-        print("You have a Half tank of gas left\nYou have 126 miles to empty")
+        print("     You have a Half tank of gas left\n You have 126 miles to empty")
     elif gasLevelIndicator == "Three Quarter Tank":
-        print("You have Three quarter tank of gas left\nYou have 210 miles to empty")
+        print("     You have Three quarter tank of gas left\n You have 210 miles to empty")
     else:
-     print("You have Full of gas left\nYou have 310 miles to empty")
+     print("     You be FULL of Gas \n You have 310 miles to empty")
 
 # Call Functions Here
 gasLevelAlert()
