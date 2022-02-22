c909abcd9b6a0e113cd3dab35d5639ed95af44a2
TylerShepard <02tyler.shepard@gulllakecs.org>
2022-02-22 10:59:58 -0800

    Time Library (Sleep)

--- InfoTechCenter3.0.py
+++ InfoTechCenter3.0.py
@@ -1,5 +1,9 @@
 # Code Name - Hornet
+#Print to one line w/ time delay between prints
+from time import sleep
 
 print("Welcome to Hornets InfoTechCenter\n")
+sleep(1.0)
 
 print("Hornet's Operating System Booting Up")
+
