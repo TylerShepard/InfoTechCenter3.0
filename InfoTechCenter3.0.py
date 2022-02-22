ff4fee6ce214f24b33a10d34f96fa21562cd3d46
TylerShepard <02tyler.shepard@gulllakecs.org>
2022-02-22 11:02:17 -0800

    Imported Colorama to change text color

--- InfoTechCenter3.0.py
+++ InfoTechCenter3.0.py
@@ -1,9 +1,13 @@
 # Code Name - Hornet
 #Print to one line w/ time delay between prints
 from time import sleep
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
 
-print("Welcome to Hornets InfoTechCenter\n")
+print(Fore.RED + "Welcome to Hornets InfoTechCenter\n")
 sleep(1.0)
 
 print("Hornet's Operating System Booting Up")
+
 
